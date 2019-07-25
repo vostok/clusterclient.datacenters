0868919729ef143356e0f0a16f5fb9e78342da20
kungurtsev <kungurtsev@skbkontur.ru>
2019-07-25 18:21:40 +0500

    code

---
+++ Vostok.ClusterClient.Datacenters/IWeighedReplicaOrderingBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using Vostok.Clusterclient.Core.Ordering.Weighed;
+using Vostok.Datacenters;
+
+namespace Vostok.ClusterClient.Datacenters
+{
+    [PublicAPI]
+    public static class IWeighedReplicaOrderingBuilderExtensions
+    {
+        /// <summary>
+        /// Adds a <see cref="AvoidInactiveDatacentersModifier"/> that will apply zero weights for non-active datacenters.
+        /// </summary>
+        public static void SetupAvoidInactiveDatacentersWeightModifier(
+            [NotNull] this IWeighedReplicaOrderingBuilder self,
+            [NotNull] IDatacenters datacenters)
+        {
+            self.AddModifier(new AvoidInactiveDatacentersModifier(datacenters));
+        }
+
+        /// <summary>
+        /// Adds a <see cref="BoostLocalDatacentersModifier"/> that will increases weight of replicas in local datacenter.
+        /// </summary>
+        public static void SetupBoostLocalDatacentersWeightModifier(
+            [NotNull] this IWeighedReplicaOrderingBuilder self,
+            [NotNull] IDatacenters datacenters,
+            double boostMultiplier = 3.0,
+            double minimumWeightForBoosting = 0.75)
+        {
+            self.AddModifier(new BoostLocalDatacentersModifier(datacenters));
+        }
+    }
+}
