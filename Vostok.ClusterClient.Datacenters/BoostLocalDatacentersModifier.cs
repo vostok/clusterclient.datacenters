ca9db27ebcc4d468d9ce772304ded3a5008d83a5
kungurtsev <kungurtsev@skbkontur.ru>
2020-02-06 16:17:29 +0500

    hot boost parameters

--- Vostok.ClusterClient.Datacenters/BoostLocalDatacentersModifier.cs
+++ Vostok.ClusterClient.Datacenters/BoostLocalDatacentersModifier.cs
@@ -15,29 +15,37 @@ namespace Vostok.ClusterClient.Datacenters
     public class BoostLocalDatacentersModifier : IReplicaWeightModifier
     {
         private readonly IDatacenters datacenters;
-        private readonly double boostMultiplier;
-        private readonly double minimumWeightForBoosting;
+        private readonly Func<double> boostMultiplierProvider;
+        private readonly Func<double> minimumWeightForBoostingProvider;
 
         public BoostLocalDatacentersModifier(
             [NotNull] IDatacenters datacenters,
-            double boostMultiplier = 3.0,
-            double minimumWeightForBoosting = 0.75)
+            double boostMultiplier = Constants.DefaultBoostMultiplier,
+            double minimumWeightForBoosting = Constants.DefaultMinimumWeightForBoosting)
+            : this(datacenters, () => boostMultiplier, () => minimumWeightForBoosting)
+        {
+        }
+
+        public BoostLocalDatacentersModifier(
+            [NotNull] IDatacenters datacenters,
+            [NotNull] Func<double> boostMultiplierProvider,
+            [NotNull] Func<double> minimumWeightForBoostingProvider)
         {
             this.datacenters = datacenters ?? throw new ArgumentNullException(nameof(datacenters));
-            this.boostMultiplier = boostMultiplier;
-            this.minimumWeightForBoosting = minimumWeightForBoosting;
+            this.boostMultiplierProvider = boostMultiplierProvider ?? throw new ArgumentNullException(nameof(boostMultiplierProvider));
+            this.minimumWeightForBoostingProvider = minimumWeightForBoostingProvider ?? throw new ArgumentNullException(nameof(minimumWeightForBoostingProvider));
         }
 
         public void Modify(Uri replica, IList<Uri> allReplicas, IReplicaStorageProvider storageProvider, Request request, RequestParameters parameters, ref double weight)
         {
-            if (weight < minimumWeightForBoosting)
+            if (weight < minimumWeightForBoostingProvider())
                 return;
 
             var localDatacenter = datacenters.GetLocalDatacenter();
             var replicaDatacenter = datacenters.GetDatacenter(replica.Host);
 
             if (string.Equals(localDatacenter, replicaDatacenter, StringComparison.OrdinalIgnoreCase))
-                weight *= boostMultiplier;
+                weight *= boostMultiplierProvider();
         }
 
         public void Learn(ReplicaResult result, IReplicaStorageProvider storageProvider)
---
+++ Vostok.ClusterClient.Datacenters/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Vostok.ClusterClient.Datacenters
+{
+    internal static class Constants
+    {
+        // Note(kungurtsev): amount of local traffic is x/(x + n - 1), where n is the number of datacenters.
+        public const double DefaultBoostMultiplier = 5;
+        
+        public const double DefaultMinimumWeightForBoosting = 0.3;
+    }
+}
